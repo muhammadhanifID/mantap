8b1473bf12de74c15ee7601c03c6b68a651c49be
muhammadhanifID <mhd.hanif.id@gmail.com>
2022-11-09 10:38:23 +0700

    api

--- functions/callback.js
+++ functions/callback.js
@@ -1,12 +1,12 @@
 const qs = require("querystring");
 exports.handler = async function (event) {
-	console.log(event.body);
+	var tes = console.log(event.body);
 	return {
 		headers: {
 			"Content-Type": "application/json",
 		},
 
 		statusCode: 200,
-		body: JSON.stringify("bisa gabs"),
+		body: JSON.stringify(event.body),
 	};
 };
